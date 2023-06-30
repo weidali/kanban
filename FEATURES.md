7e59d7b66a2325c81e32dafc3f5c510d78d40ece
Vitaliy <vito.co@ya.ru>
2023-06-30 16:45:48 +0300

    - wip: refactoring

--- FEATURES.md
+++ FEATURES.md
@@ -32,3 +32,34 @@ GenServer.call(pid, :state)                
 
 ```
 
+```zsh
+alias Kanban.TaskFSM, as: F
+F.start_link %Task{state: :idle} 
+#=> {:ok, #PID<0.198.0>}
+
+Process.alive? self()
+#=> true
+F.state
+#=> :idle
+F.finish
+#=> {:error, {:not_allowed, :finish, :idle}}
+
+F.start
+#=> :ok
+Process.alive? self()
+#=> true
+F.state
+#=> :doing
+
+F.finish
+#=> :ok
+Process.alive? self()
+#=> false
+GenServer.call(pid, :state)                
+#=> ** (exit) exited in: GenServer.call(#PID<0.210.0>, :state, 5000)
+    ** (EXIT) no process: the process is not alive or there's no process currently associated with the given name, possibly because its application isn't started
+    (elixir 1.15.0) lib/gen_server.ex:1074: GenServer.call/3
+    iex:10: (file)
+
+```
+
--- lib/kanban/task_fsm.ex
+++ lib/kanban/task_fsm.ex
@@ -11,33 +11,42 @@ defmodule Kanban.TaskFSM do
     GenServer.start_link(__MODULE__, task, name: __MODULE__)
   end
 
-  @impl GenServer
+  def start(pid \\__MODULE__) do
+    GenServer.call(pid, {:transition, :start})
+  end
 
+  def finish(pid \\__MODULE__) do
+    GenServer.call(pid, {:transition, :finish})
+  end
+
+  def state(pid \\__MODULE__) do
+    GenServer.call(pid, :state)
+  end
+
+  @impl GenServer
   def init(state), do: {:ok, state}
 
+  @impl GenServer
   def handle_call(:state, _from, %Task{state: state} = task) do
     {:reply, state, task}
   end
 
   # :idle, :doing, :done
+  @impl GenServer
   def handle_call({:transition, :start}, _from, %Task{state: :idle} = task) do
     # task = Task.update(task) or something else
     {:reply, :ok, %Task{task | state: :doing}}
   end
 
+  @impl GenServer
   def handle_call({:transition, :finish}, _from, %Task{state: :doing} = task) do
     # Save to external storage or doing something
     {:stop, :normal, :ok, %Task{task | state: :done}}
   end
 
+  @impl GenServer
   def handle_call({:transition, transition}, _from, %Task{state: state} = task) do
     # IO.inspect({transition, _from, state, task}, label: "CALL")
     {:reply, {:error, {:not_allowed, transition, state}}, task}
-  end
-
-  @impl GenServer
-  def handle_call({:+, value}, _from, state) do
-    state = state + value
-    {:reply, state, state}
   end
 end

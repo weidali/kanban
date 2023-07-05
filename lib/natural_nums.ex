828c336457f6b5ba9524ab3d7507e1a342cc8a9d
Vitaliy <vito.co@ya.ru>
2023-07-05 16:14:39 +0300

    - ch_03: refuctoring narural numbers

--- lib/natural_nums.ex
+++ lib/natural_nums.ex
@@ -1,8 +1,17 @@
 defmodule NaturalNums do
   def print(1), do: IO.puts(1)
+  def print(0), do: IO.puts(0)
 
-  def print(n) do
+  def print(n) when is_number(n) and n > 0 do
     print(n - 1)
     IO.puts(n)
+  end
+
+  def print(n) when is_number(n) and n < 0 do
+    IO.puts("Negative number: #{n}")
+  end
+
+  def print(n) when not is_number(n) do
+    IO.puts("Not number: #{n}")
   end
 end

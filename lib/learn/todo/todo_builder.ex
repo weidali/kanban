ed902ebd87992e6b38cda9d7d2b367389f55a32a
Vitaliy <vito.co@ya.ru>
2023-07-08 09:42:34 +0300

    - ch_04: create a new list iteratively

---
+++ lib/learn/todo/todo_builder.ex
@@ -0,0 +1,11 @@
+defmodule TodoList do
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      fn entry, todo_list_acc ->
+        add_entry(todo_list_acc, entry)
+      end
+    )
+  end
+end

dc48f38e34b445279c6196f99b18ae3b8b10c6d4
Vitaliy <vito.co@ya.ru>
2023-07-08 09:49:24 +0300

    - ch_04: add entry

--- lib/learn/todo/todo_builder.ex
+++ lib/learn/todo/todo_builder.ex
@@ -1,11 +1,18 @@
 defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
   def new(entries \\ []) do
     Enum.reduce(
       entries,
       %TodoList{},
-      fn entry, todo_list_acc ->
-        add_entry(todo_list_acc, entry)
-      end
+      &add_entry(&2, &1)
     )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 end

b31be99294ed387ce2b2cc909a522276efdbb802
Vitaliy <vito.co@ya.ru>
2023-07-23 14:14:32 +0300

    - custom GenServer: One-Way Calls

--- lib/learn/todo/server_process.ex
+++ lib/learn/todo/server_process.ex
@@ -31,6 +31,18 @@ defmodule ServerProcess do
 end
 
 defmodule KeyValueStore do
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
   def init do
     %{}
   end

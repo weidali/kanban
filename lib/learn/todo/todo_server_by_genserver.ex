7edbad8d898d56e0ee904a23ade532128fdc531b
Vitaliy <vito.co@ya.ru>
2023-07-23 17:14:38 +0300

    - wip: refactoring

--- lib/learn/todo/todo_server_by_genserver.ex
+++ lib/learn/todo/todo_server_by_genserver.ex
@@ -45,11 +45,16 @@ end
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new(), do: %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
 
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
-
     new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
 
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
@@ -71,25 +76,13 @@ defmodule TodoList do
         todo_list
 
       {:ok, old_entry} ->
-        old_entry_id = old_entry.id
-        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entry = updater_fun.(old_entry)
         new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
   end
 
   def delete_entry(todo_list, entry_id) do
-    case Map.fetch(todo_list.entries, entry_id) do
-      :error ->
-        todo_list
-
-      {:ok, old_entry} ->
-        %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
-    end
-  end
-
-  def due_today(todo_list) do
-    today = Date.utc_today()
-    entries(todo_list, today)
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
